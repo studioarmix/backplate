b5a4737fa779b3a1a8f6667efd27ac453562a3e5
Peter Boyer <petertboyer@gmail.com>
2017-05-01 02:43:14 +1000

    Setup test, license added, folder structure correction

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from distutils.core import setup
 
 setup(
     name='jumpstartapi',
-    version='1.0',
-    description='StudioArmix RESTful API Startup Utils',
+    version='0.1',
+    description='RESTful API Jumpstart Utilities',
     author='Peter Boyer',
     author_email='petertboyer@gmail.com',
     url='https://github.com/studioarmix/jumpstart-api',
-    packages=[
+    packages=['jumpstartapi'],
+    license='MIT',
+    install_requires=[
         'flask',
         'flask-restful',
+        'sqlalchemy',
         'requests',
         'marshmallow',
         'webargs',
