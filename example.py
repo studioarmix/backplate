efcc7a7d189b9909ea8402388f375406a9f91d5f
Peter Boyer <petertboyer@gmail.com>
2017-06-24 18:01:44 +1000

    Update example

--- example.py
+++ example.py
@@ -1,61 +1,27 @@
 
-from backbone import (
-    create_api, endpoint,
-    APIError, errordef
-)
-
 from flask import Flask
 from flask_restful import Resource
-
-ERR_FIELDS_INVALID = 'ERR_FIELDS_INVALID'
-ERR_EMAIL_ALREADY_EXISTS = 'ERR_EMAIL_ALREADY_EXISTS'
-ERR_MY_ERROR = 'ERR_MY_ERROR'
+from backbone import create_api, endpoint
 
 class Profile(Resource):
     def get(self):
-        return {
-            'id': 1
-        }
+        return {'id': 1}
 
-class Business(Resource):
-    def get(self, id):
-        if int(id) > 19:
-            raise APIError(ERR_MY_ERROR, data={
-                'fields': {'username': 'Incorrect length.'}})
-
-        return {
-            'id': id
-        }
-
-class Businesses(Resource):
+class Items(Resource):
     def get(self):
         return [{'id': idx} for idx in range(0, 20)]
 
+class Item(Resource):
+    def get(self, id):
+        return {'id': id}
+
 endpoints = [
-    endpoint('profile', '/profile', Profile),
-    endpoint('business', '/businesses', Businesses, Business)
-]
-
-decorators = []
-
-errors = [
-    errordef(ERR_FIELDS_INVALID,
-             422, 'One or more fields raised validation errors.'),
-    errordef(ERR_EMAIL_ALREADY_EXISTS,
-             409, 'An account already exists with this email.'),
-    errordef(ERR_MY_ERROR,
-             422, 'My Awesome Error')
+    endpoint('me', '/me', Profile),
+    endpoint('item', '/items', Items, Item)
 ]
 
 app = Flask(__name__)
-
-create_api(
-    'api', __name__,
-    endpoints=endpoints,
-    decorators=decorators,
-    errors=errors,
-    app=app
-)
+create_api(__name__, app=app, endpoints=endpoints)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
